90e9549bcb5069e0b2083471ca6ee889826ac5d3
Nahuel Garcia <nhlgrc@hotmail.com>
2024-01-25 17:52:36 -0300

    aaa

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const handleButton = ( valor ) => {
-    //document.querySelector('#contador').innerHTML = Number(document.querySelector('#contador').innerHTML) + valor;
-    value += valor;
-}
+
 
 
 export const CounterApp = ( {value} ) => {
 
-    const [ counter ] = useState( 15 );
+    const valorInicial = 15;
+
+    //counter es el valor y setCounter la función para modificarlo y renderizarlo
+    const [ counter, setCounter ] = useState( valorInicial );
+
+    const handleButton = ( valor ) => {
+        //document.querySelector('#contador').innerHTML = Number(document.querySelector('#contador').innerHTML) + valor;
+        setCounter( counter + valor );
+    }
 
     return (
     <>
