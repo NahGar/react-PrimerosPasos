64cf15ee0a480c797feab26cf0819655168ab8f1
Nahuel Garcia <nhlgrc@hotmail.com>
2024-02-02 16:49:03 -0300

    asdsad

---
+++ tests/base-pruebas/11-async-await.test.js
@@ -0,0 +1,15 @@
+import { getImagen } from "../../src/base-pruebas/11-async-await";
+
+describe('Pruebas en 11-async-await', () => {
+
+    test('getImagen debe retornar una URL de la imagen', async() => {
+
+        const url = await getImagen();
+
+        //console.log(url);
+
+        expect( url ).toContain('https://');
+
+    });
+
+});
