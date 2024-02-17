fcb943834f01782972a3025f3d2b6fabec9993f0
Nahuel Garcia <nhlgrc@hotmail.com>
2024-02-17 11:24:13 -0300

    aaa

--- tests/base-pruebas/11-async-await.test.js
+++ tests/base-pruebas/11-async-await.test.js
@@ -1,6 +1,5 @@
 import { getImagen } from "../../src/base-pruebas/11-async-await";
 
-<<<<<<< HEAD
 describe('Pruebas en 11-async-await.js', () => {
 
     test('getImagen debe retornar URL', async() => {
@@ -9,18 +8,5 @@ describe('Pruebas en 11-async-await.js', () => {
         console.log(resp);
 
         expect( resp ).not.toBe( 'No se encontro la imagen' );
-=======
-describe('Pruebas en 11-async-await', () => {
-
-    test('getImagen debe retornar una URL de la imagen', async() => {
-
-        const url = await getImagen();
-
-        //console.log(url);
-
-        expect( url ).toContain('https://');
->>>>>>> 64cf15ee0a480c797feab26cf0819655168ab8f1
-
     });
-
 });
